12b89691cbdcf0224f3c57321b582d2d6d6dc14d
MarioMarcos <59515079+MarioMarcos@users.noreply.github.com>
2020-01-22 11:36:24 -0300

    delete lines ACESSO EXADATA

--- ACESSO EXADATA.sql
+++ ACESSO EXADATA.sql
@@ -1,19 +1,4 @@
-Teste de alteração para visualização do Aurélio.
 
-1-log no entreposto linux1 
-2-log com ssh cpf e senha ldap no ip 10.193.34.96 - LDAP
-3-execute:  sudo su - dwpub
-4-depois disso acesse com sqlplus o banco que você precisa.
-5-sqlplus dwpub@whpro130 - Produção
-Senha dwpubpubexa
-
-6-se você for fazer publicação acesse o diretório pro_21761_dwcorp dentro dele ok.
-
-7-para acessar o banco de homologação são paulo é só sqlplus dwpub@whpre130 com senha igual do anterior.
-
-
-obs: para criar o spool do oracle ir para pasta do banco tipo pre ou pro e ir para pasta work e de lá chamar o sql ai poderá gravar belezinha
-     o spool 
 
 fizemos diferente André configurou para funcionar com o DWPUB e ai está legal ...
 --===========================================================================================================
