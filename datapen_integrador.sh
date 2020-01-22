f3a2011b259ff6914ef6d412042b5e2cfde4a0d3
MarioMarcos <59515079+MarioMarcos@users.noreply.github.com>
2020-01-22 11:21:12 -0300

    Add files via upload

---
+++ datapen_integrador.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+HOME_INTEGRADOR=/opt/websites/tre_10724_sisdepen-integrador
+PAR_LOG_DIR=/opt/approtinas/tre_10724_sisdepen-integrador/logs
+PAR_CONFIG=/opt/appconf/tre_10724_sisdepen-integrador/config/integrador.properties
+PAR_UF=AC,AM,RN,SC,TO
+
+set -e  # Interrompe execução imediatamente em caso de algum comando com erro.
+java -jar "$HOME_INTEGRADOR/datapen-integracao.jar" dados.uf="$PAR_UF" filtrar.por.uf=true logBasePath="$PAR_LOG_DIR" config="$PAR_CONFIG"
+
+
+
+
+
+
